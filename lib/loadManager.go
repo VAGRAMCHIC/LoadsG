7a1a51c0d10efcadda08390b90ef1c019fdefe6e
krbashyan <vagramchic@gmail.com>
2025-08-12 11:38:56 +0300

    add dev branch

--- lib/loadManager.go
+++ lib/loadManager.go
@@ -7,14 +7,6 @@ import (
 )
 
 func RunLoad(load HTTPLoadRequest) (string, error) {
-	//headers := map[string]string{
-	//	"Content-Type": "application/x-www-form-urlencoded",
-	//	"User-Agent":   "LoadsG/1.0",
-	//}
-	//var httpLoadRequest HTTPLoadRequest
-	//httpLoadRequest.Id = 0
-	//httpLoadRequest.HttpHead = CreateHttpHead("GET", "http://test.customlabs.ru/test2/", "HTTP/1.1", headers)
-	//httpLoadRequest.Body = "test"
 	request, host := BuildHttpRequest(load.HttpHead, load.Body)
 	requests := make([]string, load.Count)
 	for i := range load.Count {
